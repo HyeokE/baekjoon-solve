920fa79b106ae4be13fd30fca9234afb3106f53b
HYEOK_E <jhjeong00@gmail.com>
2021-05-17 20:03:15 +0900

    16917 clear 16922ing

--- Q16917.java
+++ Q16917.java
@@ -7,26 +7,18 @@ public class Q16917 {
         int default_chic = sc.nextInt(); //후라이드 가격
         int half_chic = sc.nextInt(); //반반 가격
         int source_num = sc.nextInt(); //양념개수
-        int default_num = sc.nextInt(); //후라이드 가격
+        int default_num = sc.nextInt(); //후라이드 개수
         int price_result = 0;
-        if (source_chic + default_chic < half_chic * 2) {
-            price_result = source_chic*source_num+default_chic*default_num;
-        } else {
-            if (source_num > default_num) { //후라이드보다 양념이 많을 경우
-                price_result = price_result + source_chic * (source_num - default_num);
-//                System.out.println(price_result);
-                price_result = price_result + ((half_chic * 2) * default_num);
-//                System.out.println(((half_chic * 2) * default_num));
-//                System.out.println(price_result);
-
-            } else if (default_num > source_num) {
-                price_result = price_result + default_chic * (default_num - source_num);
-//                System.out.println(price_result);
-                price_result = price_result + ((half_chic * 2) * source_num);
-//                System.out.println(price_result);
-
-            } else if (default_num == source_num) {
-                price_result = price_result + source_num * (half_chic * 2) * source_num;
+        if ((source_chic + default_chic) <= half_chic * 2) {//반반이랑 양념+후라이드 비교
+            price_result += (source_chic * source_num) + (default_chic * default_num);
+        } else { //후라이드 + 양념가 더 크면
+            if (source_num > default_num) {//양념이 후라이드보다 크면
+                price_result += default_num * (half_chic * 2); //반반가격x2x후라이드개수에다가 양념개수에서 후라이드 개수 뺀 가격만큼 더해준다.
+                price_result += Math.min((source_num - default_num) * source_chic, (source_num - default_num) * (half_chic * 2));
+                //(양념 - 후라이드개수) X (양념가격과 양념 - 후라이드) X 반반치킨 X 2 비교해서 더 작은 값
+            } else {
+                price_result += source_num * (half_chic * 2);
+                price_result += Math.min((default_num - source_num) * default_chic, (default_num - source_num) * (half_chic * 2));
             }
 
         }
---
+++ Q16922.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q16922 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        int get_num  = sc.nextInt();
+
+        int num [] = {1,5,10,50};
+        boolean check [] = new boolean[1001]; //가장 큰 수가 1000이라 1001까지 생성
+
+    }
+}
